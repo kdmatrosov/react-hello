342fde15173f82d03f14408a25b5221cb2a454ce
Kirill Matrosov <kdmatrosov@gmail.com>
2016-02-28 18:31:33 +0300

    вывод значения в вертску через функцию

--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -18,8 +18,12 @@ var TweetBox = React.createClass({
   onTextAreaChange: function(event) {
 	  //навесить на событие onChange - это событие реакта, как ng-change в ангуляре
 	  //указывать событие надо через this, чтобы была видна принадлежность именно к этому компоненту, как в хомяке
-	  this.setState({ text: event.target.value });
+	  this.setState({ text: event.target.value }); //при такой установке значения, если в state больше одного ключа, остальные не удаляются
 	  },
+  getTextLength: function()
+  {
+	return this.state.text.trim();  
+  },
   render: function() {
 	  //в случае disabled опять проверка по жс внутри верстки....
     return (
@@ -27,7 +31,8 @@ var TweetBox = React.createClass({
 		<p>
 			<textarea className="form-control" onChange={this.onTextAreaChange}></textarea>
         </p>
-		<p>
+		<p> 
+			<span>{this.getTextLength()}</span>
 			<button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
         </p>
       </div>
