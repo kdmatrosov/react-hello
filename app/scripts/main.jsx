a4a5d9c914a2058e5f935d0ef484658e17a481ea
Kirill Matrosov <kdmatrosov@gmail.com>
2016-02-28 18:23:04 +0300

    описание начало работы со state

--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -3,16 +3,32 @@ import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router'
 
 const node = document.getElementById('content');
-
+/*основное отличие реакта от джейквери в том, что меняется не dom, а состояние (state)
+каждый раз при изменении state, автоматически запускается функция render
+внутри render у нас есть доступ к state
+*/
 var TweetBox = React.createClass({
+	
+  getInitialState: function() {
+	  //специальный метод инициализации состояния 
+	return {
+      text: ""
+    };
+  },
+  onTextAreaChange: function(event) {
+	  //навесить на событие onChange - это событие реакта, как ng-change в ангуляре
+	  //указывать событие надо через this, чтобы была видна принадлежность именно к этому компоненту, как в хомяке
+	  this.setState({ text: event.target.value });
+	  },
   render: function() {
+	  //в случае disabled опять проверка по жс внутри верстки....
     return (
       <div className="well clearfix">
 		<p>
-			<textarea className="form-control"></textarea>
+			<textarea className="form-control" onChange={this.onTextAreaChange}></textarea>
         </p>
 		<p>
-			<button className="btn btn-primary pull-right">Tweet</button>
+			<button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
         </p>
       </div>
     );
